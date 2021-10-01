f83d7307cf7a8b4062ce90163ce9c4ce5e89553d
Aleksander Piotrowski <apiotrowski312@gmail.com>
2021-10-01 07:39:44 +0200

    Add pubsub

---
+++ pubsub/pubsub.go
@@ -0,0 +1,63 @@
+package pubsub
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/golang/protobuf/proto"
+)
+
+func SetupPubsubTopic(ctx context.Context, project, topicID string, subIDs ...string) error {
+	c, err := pubsub.NewClient(ctx, project)
+	if err != nil {
+		return fmt.Errorf("failed to create pubsub client: %w", err)
+	}
+	topic, err := ensureTopic(ctx, c, topicID)
+	if err != nil {
+		return fmt.Errorf("failed to ensure that topic %q exists: %w", topicID, err)
+	}
+	for _, sub := range subIDs {
+		_, err := ensureSubscription(ctx, c, topic, sub)
+		if err != nil {
+			return fmt.Errorf("failed to create subscription %q: %w", sub, err)
+		}
+	}
+	return nil
+}
+
+func MustSetupPubSubTopic(ctx context.Context, project, topicID string, subIDs ...string) {
+	err := SetupPubsubTopic(ctx, project, topicID, subIDs...)
+	if err != nil {
+		log.Fatalf("Failed to setup pubsub for topic %q: %v", topicID, err)
+	}
+}
+
+//MustSetupPubSubReceiver is used to create PubSub receiver.
+// User must ensure that topic is created.
+func MustSetupPubSubSubscription(ctx context.Context, projectID, topicID, subID string, cb func(context.Context, *pubsub.Message)) {
+	r, err := NewSubscription(ctx, projectID, topicID, subID)
+	if err != nil {
+		log.Fatalf("Failed to create receiver for topic %q: %v", topicID, err)
+	}
+
+	go func() {
+		err := r.Receive(ctx, cb)
+		if err != nil {
+			log.Fatalf("Failed to start receiver for topic %q: %v", topicID, err)
+		}
+	}()
+}
+
+//MustPublishPubsubMsg is a helper function used to send custom PubSub message.
+func MustPublishPubsubMsg(t *testing.T, ctx context.Context, s *pubsub.Topic, msg proto.Message) {
+	res := s.Publish(ctx, &pubsub.Message{
+		Data: []byte(msg.String()),
+	})
+	_, err := res.Get(ctx)
+	if err != nil {
+		t.Fatalf("Failed to publish message to pubsub: %v", err)
+	}
+}
---
+++ pubsub/utils.go
@@ -0,0 +1,82 @@
+package pubsub
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"cloud.google.com/go/pubsub"
+)
+
+func NewSubscription(ctx context.Context, projectID, topicID, subscriptionID string) (*pubsub.Subscription, error) {
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client for project %q: %v", projectID, err)
+	}
+
+	topic := client.Topic(topicID)
+	ok, err := topic.Exists(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check if topic exists: %w", err)
+	}
+	if !ok {
+		return nil, fmt.Errorf("topic %q does not exist", topicID)
+	}
+
+	subscription, err := ensureSubscription(ctx, client, topic, subscriptionID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to subscription %q: %w", subscriptionID, err)
+	}
+
+	return subscription, nil
+}
+
+func NewTopic(ctx context.Context, projectID, topic string) (*pubsub.Topic, error) {
+	c, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create pubsub client: %w", err)
+	}
+	t := c.Topic(topic)
+	ok, err := t.Exists(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check if topic exists: %w", err)
+	}
+	if !ok {
+		return nil, fmt.Errorf("topic %q does not exists", topic)
+	}
+	return t, nil
+}
+
+func ensureTopic(ctx context.Context, cli *pubsub.Client, topicID string) (*pubsub.Topic, error) {
+	t := cli.Topic(topicID)
+	ok, err := t.Exists(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check if topic exists: %w", err)
+	}
+	if ok {
+		return t, nil
+	}
+	t, err = cli.CreateTopic(ctx, topicID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create topic %q: %w", topicID, err)
+	}
+	return t, nil
+}
+
+func ensureSubscription(ctx context.Context, cli *pubsub.Client, t *pubsub.Topic, subID string) (*pubsub.Subscription, error) {
+	s := cli.Subscription(subID)
+	ok, err := s.Exists(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check if subscription %q exists: %w", subID, err)
+	}
+	if ok {
+		return s, nil
+	}
+	_, err = cli.CreateSubscription(ctx, subID, pubsub.SubscriptionConfig{
+		Topic: t,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create subscription %q: %w", subID, err)
+	}
+	return s, nil
+}
