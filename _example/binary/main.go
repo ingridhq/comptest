35a74c6db94e753b1e2972f490cd36e1e85c31cc
Aleksander Piotrowski <apiotrowski312@gmail.com>
2021-12-03 08:44:12 +0100

    Add better description and some logs

--- _example/binary/main.go
+++ _example/binary/main.go
@@ -11,17 +11,20 @@ import (
 )
 
 func main() {
+	log.Println("START")
 	var cfg config.Config
 	envconfig.MustProcess("", &cfg)
 
 	mainMux := http.NewServeMux()
 	metricsMux := http.NewServeMux()
 	mainMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Potato endpoint")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Potato"))
 	})
 
 	metricsMux.HandleFunc("/readiness", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Readiness endpoint")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("ready!!!"))
 	})
@@ -29,11 +32,13 @@ func main() {
 	go func() {
 		// Sleep to simulate system is not ready to use
 		time.Sleep(time.Second * 1)
+		log.Println("Start metrics server")
 		if err := http.ListenAndServe(cfg.MetricPort, metricsMux); err != nil {
 			log.Fatal(err)
 		}
 	}()
 
+	log.Println("Start main server")
 	if err := http.ListenAndServe(cfg.Port, mainMux); err != nil {
 		log.Fatal(err)
 	}
